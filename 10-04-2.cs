a67aeac868e00678b5f508ef45caef290667d5ac
thiagocaleffi <thiagocaleffi.artes@gmail.com>
2025-04-10 14:28:52 -0300

    Add files via upload

---
+++ 10-04-2.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+string[] carros = { "Volvo", "Peugeot", "Nissan"};
+
+carros[1] = "Opel";
+string novamarca = "Toyota";
+List<string> novocarros = new List<string>(carros.ToList());
+novocarros.Add(novamarca);
+carros = novocarros.ToArray();  
+Array.Sort(carros);
+
+foreach (var item in carros)
+{
+    WriteLine(item.ToUpper());
+}
+
+ReadLine();
