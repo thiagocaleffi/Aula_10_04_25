4ec7fefcbef7a2665b065fe3794734d92a372509
thiagocaleffi <thiagocaleffi.artes@gmail.com>
2025-04-10 14:19:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+string[] carros = { "Volvo", "Peugeot", "Nissan"};
+
+carros[1] = "Opel";
+Array.Sort(carros);
+
+foreach (var item in carros)
+{
+    WriteLine(item.ToUpper());
+}
+
+ReadLine();
